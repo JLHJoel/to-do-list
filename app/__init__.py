a54cca2b4a2dab99fd4175e4e4c7c34f9abe24de
JoseeMoreno <jose.moreno39@uabc.edu.mx>
2025-05-03 15:36:00 -0700

    Version con register

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
+import random
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -13,13 +15,12 @@ def create_app(config_class=Config):
     
     db.init_app(app)
     login_manager.init_app(app)
-    
+
     from app.routes import main
     app.register_blueprint(main)
     
     with app.app_context():
         db.create_all()
-        # Crear usuario predefinido si no existe
         from app.models import User
         if not User.query.filter_by(username='admin').first():
             user = User(username='admin')
@@ -28,3 +29,10 @@ def create_app(config_class=Config):
             db.session.commit()
     
     return app
+
+
+from flask import current_app
+
+@db.event.listens_for(db.engine, "connect")
+def inject_random_id(*args, **kwargs):
+    current_app.jinja_env.globals['random_id'] = random.randint(1, 10000)
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from app.models import User, Task
 from app import db
-import requests
+import random
 
 main = Blueprint('main', __name__)
 
@@ -34,19 +34,9 @@ def logout():
 @main.route('/tasks')
 @login_required
 def tasks():
-    # Obtener una frase motivacional
-    try:
-        response = requests.get('https://api.quotable.io/random')
-        if response.status_code == 200:
-            quote_data = response.json()
-            quote = f'"{quote_data["content"]}" - {quote_data["author"]}'
-        else:
-            quote = "El éxito es la suma de pequeños esfuerzos repetidos día tras día."
-    except:
-        quote = "El éxito es la suma de pequeños esfuerzos repetidos día tras día."
-    
     tasks = Task.query.filter_by(user_id=current_user.id).all()
-    return render_template('tasks.html', tasks=tasks, quote=quote)
+    return render_template('tasks.html', tasks=tasks)
+
 
 @main.route('/add_task', methods=['POST'])
 @login_required
@@ -75,3 +65,28 @@ def delete_task(id):
         db.session.delete(task)
         db.session.commit()
     return redirect(url_for('main.tasks'))
+
+
+#nuevo
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.tasks'))
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('El nombre de usuario ya está registrado.')
+            return redirect(url_for('main.register'))
+
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Usuario registrado correctamente. Ahora puedes iniciar sesión.')
+        return redirect(url_for('main.login'))
+
+    return render_template('register.html')
