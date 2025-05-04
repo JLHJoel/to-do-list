c287294aab9b736b82f6140b10b8152411608bd8
JLHJoel <103686406+JLHJoel@users.noreply.github.com>
2025-05-03 19:04:21 -0700

    Delete to-do-list (incluye archivo de pruebas unitarias)/to-do-list directory

--- to-do-list (incluye archivo de pruebas unitarias)/to-do-list/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    SECRET_KEY = 'clave-secreta-muy-segura'
-    # Aquí usamos SQLite para simplificar
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
